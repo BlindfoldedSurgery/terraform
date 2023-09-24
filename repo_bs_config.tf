73030c211f338c8103c800fa83369a1ce9e8866c
Björn Petersen <git@bjoernpetersen.net>
2023-09-24 22:42:52 +0200

    Update status checks for bs-config

--- repo_bs_config.tf
+++ repo_bs_config.tf
@@ -2,4 +2,10 @@ module "bs_config_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module"
   name        = "bs-config"
   description = "[WIP] Config package for Python"
+  required_status_checks = [
+    "test (3.10)",
+    "test (3.11)",
+    "lint (3.10)",
+    "lint (3.11)",
+  ]
 }
