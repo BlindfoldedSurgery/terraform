68d600eab43d09a0d3c70b8a5d147cbed6a14325
Björn Petersen <git@bjoernpetersen.net>
2023-09-25 21:17:16 +0200

    Update bs-config description

--- repo_bs_config.tf
+++ repo_bs_config.tf
@@ -1,7 +1,7 @@
 module "bs_config_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module"
   name        = "bs-config"
-  description = "[WIP] Config package for Python"
+  description = "Config package for Python"
   required_status_checks = [
     "test (3.10)",
     "test (3.11)",
