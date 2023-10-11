379e0fc70f1921931bf4de0d57f7e3f40cfb49b3
Torben Carstens <git@carstens.tech>
2023-10-11 16:09:44 +0200

    Add repo/isso

---
+++ repo_isso.tf
@@ -0,0 +1,10 @@
+module "isso_repo" {
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name        = "isso"
+  description = "isso comments for eiersaft.me"
+  required_status_checks = [
+    "lint",
+    "build-container-image",
+  ]
+  create_default_branch = false
+}
