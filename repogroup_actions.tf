67c33910841249ad068290bad5b6a4ccd39390c7
Björn Petersen <git@bjoernpetersen.net>
2023-10-19 19:22:12 +0200

    Rename module

--- repogroup_actions.tf
+++ repogroup_actions.tf
@@ -1,4 +1,4 @@
-module "actions_repo" {
+module "actions_python_repo" {
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module"
   name                   = "actions-python"
   description            = "Reusable Python GitHub Actions Workflows"
