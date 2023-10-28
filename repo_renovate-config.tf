945938cc53a230d6247efd96c4652d55776b8a39
Björn Petersen <git@bjoernpetersen.net>
2023-10-28 21:13:40 +0200

    Enable Actions for renovate-config

--- repo_renovate-config.tf
+++ repo_renovate-config.tf
@@ -1,7 +1,8 @@
 module "renovate_config_repo" {
-  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.0.1"
-  name                   = "renovate-config"
-  description            = "Config presets for Renovate"
-  enable_actions         = false
-  required_status_checks = []
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.0.1"
+  name        = "renovate-config"
+  description = "Config presets for Renovate"
+  required_status_checks = [
+    "validate",
+  ]
 }
