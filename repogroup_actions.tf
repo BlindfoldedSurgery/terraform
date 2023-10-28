e55321c67f5786baa4a23a47a6fd2124eeff1727
Björn Petersen <git@bjoernpetersen.net>
2023-10-29 00:47:08 +0200

    Revert "Allow protection bypass in actions-releases"
    This reverts commit 388a639e2f78f732d6a0dd3aebdacb8843495382.

--- repogroup_actions.tf
+++ repogroup_actions.tf
@@ -2,7 +2,7 @@ module "actions_actions_repo" {
   source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
   name                                   = "actions-releases"
   description                            = "Reusable Release Workflows for GitHub Actions"
-  allow_default_branch_protection_bypass = true
+  allow_default_branch_protection_bypass = false
   required_status_checks = [
     "commitizen / check-commits"
   ]
