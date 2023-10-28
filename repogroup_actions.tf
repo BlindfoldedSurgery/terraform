fe808e3c032648da51936d45694f4ba6c00eb214
Björn Petersen <git@bjoernpetersen.net>
2023-10-28 23:14:32 +0200

    Add actions-actions

--- repogroup_actions.tf
+++ repogroup_actions.tf
@@ -1,10 +1,16 @@
+module "actions_actions_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
+  name                   = "actions-actions"
+  description            = "Reusable GitHub Actions Workflows for GitHub Actions"
+  required_status_checks = []
+}
+
 module "actions_container_repo" {
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
   name                   = "actions-container"
   description            = "Reusable Container Workflows for GitHub Actions"
   required_status_checks = []
 }
-
 
 module "actions_python_repo" {
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
