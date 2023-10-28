890823474929b07f9e9ddf394f85c8854c2fd978
Björn Petersen <git@bjoernpetersen.net>
2023-10-28 23:08:58 +0200

    Formatting

--- repo_terraform_repo_module.tf
+++ repo_terraform_repo_module.tf
@@ -1,7 +1,7 @@
 module "terraform_repo_module_repo" {
-  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
-  name        = "terraform-repo-module"
-  description = "Terraform module that allows configuration of GitHub repositories"
+  source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
+  name                                   = "terraform-repo-module"
+  description                            = "Terraform module that allows configuration of GitHub repositories"
   allow_default_branch_protection_bypass = false
   required_status_checks = [
     "fmt",
