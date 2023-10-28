016afe9990431a2aa1454b3558149753c55f0f07
Björn Petersen <git@bjoernpetersen.net>
2023-10-28 23:07:02 +0200

    Add terraform-repo-module

---
+++ repo_terraform_repo_module.tf
@@ -0,0 +1,11 @@
+module "terraform_repo_module_repo" {
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
+  name        = "terraform-repo-module"
+  description = "Terraform module that allows configuration of GitHub repositories"
+  allow_default_branch_protection_bypass = false
+  required_status_checks = [
+    "fmt",
+    "validate",
+    "check-commits"
+  ]
+}
