5e0582171b943705df9cb4df927b5e105f315177
Björn Petersen <git@bjoernpetersen.net>
2023-11-06 20:53:33 +0100

    Add required status checks for container-poetry

--- repogroup_container.tf
+++ repogroup_container.tf
@@ -2,5 +2,15 @@ module "container_poetry_repo" {
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
   name                   = "container-poetry"
   description            = "[Experimental] Container images with Poetry"
-  required_status_checks = []
+  required_status_checks = [
+    "check / check",
+    "build-container-image (3.11, bullseye, installer) / build",
+    "build-container-image (3.12, bullseye, installer) / build",
+    "build-container-image (3.11, bookworm, installer) / build",
+    "build-container-image (3.12, bookworm, installer) / build",
+    "build-container-image (3.11, bullseye, pipx) / build",
+    "build-container-image (3.12, bullseye, pipx) / build",
+    "build-container-image (3.11, bookworm, pipx) / build",
+    "build-container-image (3.12, bookworm, pipx) / build",
+  ]
 }
