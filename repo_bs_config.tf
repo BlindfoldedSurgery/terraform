747edafed3cc459971b9370f1cc72e89a09a25c1
Björn Petersen <git@bjoernpetersen.net>
2023-11-06 18:24:11 +0100

    Require PRs for bs-state and bs-config

--- repo_bs_config.tf
+++ repo_bs_config.tf
@@ -1,8 +1,10 @@
 module "bs_config_repo" {
-  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
-  name        = "bs-config"
-  description = "Config package for Python"
+  source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
+  name                                   = "bs-config"
+  description                            = "Config package for Python"
+  allow_default_branch_protection_bypass = false
   required_status_checks = [
+    "check-commits / check",
     "test (3.11) / test",
     "test (3.12) / test",
     "lint (3.11) / lint",
--- repo_bs_state.tf
+++ repo_bs_state.tf
@@ -1,8 +1,10 @@
 module "bs_state_repo" {
-  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
-  name        = "bs-state"
-  description = "State package for Python"
+  source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
+  name                                   = "bs-state"
+  description                            = "State package for Python"
+  allow_default_branch_protection_bypass = false
   required_status_checks = [
+    "check-commits / check",
     "test (3.11)",
     "lint (3.11) / lint",
     "codecov/patch",
