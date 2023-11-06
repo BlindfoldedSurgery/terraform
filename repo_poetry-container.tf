7348d6024e4e5333bd4745a48bc9e9a3667f93e1
Björn Petersen <git@bjoernpetersen.net>
2023-11-06 19:03:12 +0100

    Rename module

--- repo_poetry-container.tf
+++ repo_poetry-container.tf
@@ -1,4 +1,4 @@
-module "poetry_container_repo" {
+module "container_poetry_repo" {
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
   name                   = "container-poetry"
   description            = "[Experimental] Container images with Poetry"
