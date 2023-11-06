f826fc0e11a56057d50609ec4174e7934916aa58
Björn Petersen <git@bjoernpetersen.net>
2023-11-06 21:03:18 +0100

    Prevent protection bypass for container-poetry

--- repogroup_container.tf
+++ repogroup_container.tf
@@ -1,7 +1,8 @@
 module "container_poetry_repo" {
-  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
-  name        = "container-poetry"
-  description = "[Experimental] Container images with Poetry"
+  source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.3.0"
+  name                                   = "container-poetry"
+  description                            = "[Experimental] Container images with Poetry"
+  allow_default_branch_protection_bypass = false
   required_status_checks = [
     "check / check",
     "build-container-image (3.11, bullseye, installer) / build",
