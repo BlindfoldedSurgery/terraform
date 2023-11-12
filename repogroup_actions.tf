7b6f6ac170eaf470923e017d880d8d9a167e1e4c
Björn Petersen <git@bjoernpetersen.net>
2023-11-12 16:44:28 +0100

    Require new test actions to pass for actions-container

--- repogroup_actions.tf
+++ repogroup_actions.tf
@@ -14,7 +14,19 @@ module "actions_container_repo" {
   description                            = "Reusable Container Workflows for GitHub Actions"
   allow_default_branch_protection_bypass = false
   required_status_checks = [
-    "check / check"
+    "check / check",
+    "build-docker-minimal / build",
+    "build-docker-all-options ({{defaultContext}}:test-build) / build",
+    "build-docker-all-options (test-build) / build",
+    "build-docker-all-options (./test-build) / build",
+    "build-kaniko-minimal / build",
+    "build-kaniko-all-options ({{defaultContext}}:test-build) / build",
+    "build-kaniko-all-options (test-build) / build",
+    "build-kaniko-all-options (./test-build) / build",
+    "build-kaniko-amd64 / build",
+    "build-docker-arm64 / build",
+    "merge-with-artifact",
+    "merge-with-outputs",
   ]
 }
 
