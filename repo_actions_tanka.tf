c41e27c140c3236366909021374f184871486c58
Torben Carstens <git@carstens.tech>
2023-12-14 23:13:25 +0100

    Create actions-tanka repo

---
+++ repo_actions_tanka.tf
@@ -0,0 +1,6 @@
+module "actions_tanka_repo" {
+  source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
+  name                                   = "actions-tanka"
+  description                            = "Github action for tanka interaction"
+  required_status_checks = []
+}
