feba3de2d14afc39a568e57f8cc0363cecfb6aac
Torben Carstens <git@carstens.tech>
2023-12-17 15:36:05 +0100

    Add shared-postgres repo

---
+++ repo_shared-postgres.tf
@@ -0,0 +1,8 @@
+module "shared-postgres_repo" {
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
+  name        = "shared-postgres"
+  description = "shared postgres database for several use-cases (e.g. staging environments)"
+  required_status_checks = [
+    "lint",
+  ]
+}
