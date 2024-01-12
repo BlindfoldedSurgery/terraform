4251872c4a8e8455b74c6cd995c62fb95d87fddb
Torben Carstens <git@carstens.tech>
2024-01-12 22:29:57 +0100

    Add 'test' as required status check to actions-tanka

--- repo_actions_tanka.tf
+++ repo_actions_tanka.tf
@@ -1,6 +1,8 @@
 module "actions_tanka_repo" {
-  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
-  name                   = "actions-tanka"
-  description            = "Github action for tanka interaction"
-  required_status_checks = []
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
+  name        = "actions-tanka"
+  description = "Github action for tanka interaction"
+  required_status_checks = [
+    "test"
+  ]
 }
