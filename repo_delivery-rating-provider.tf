04aaa6c404f63b0984b28e48f50b3b9f9d3143c0
Torben Carstens <git@carstens.tech>
2024-02-29 17:33:30 +0100

    Update required_status_checks for delivery_rating_provider

--- repo_delivery-rating-provider.tf
+++ repo_delivery-rating-provider.tf
@@ -3,6 +3,6 @@ module "delivery_rating_provider_repo" {
   name        = "delivery-rating-provider"
   description = "provide suggestions for a delivery place to try"
   required_status_checks = [
-    "build-container-image",
+    "build-container-image / build",
   ]
 }
