906ba42f6fca09dc9b883f16483ad9872fc20b83
Björn Petersen <git@bjoernpetersen.net>
2024-06-30 10:47:35 +0200

    Add container-flutter

--- repogroup_container.tf
+++ repogroup_container.tf
@@ -1,3 +1,15 @@
+module "container_flutter_repo" {
+  source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
+  name                                   = "container-flutter"
+  description                            = "Container images with Flutter"
+  allow_default_branch_protection_bypass = false
+  required_status_checks = [
+    "check-commits / check",
+    "build-container-image (amd64) / build",
+    "build-container-image (arm64) / build",
+  ]
+}
+
 module "container_poetry_repo" {
   source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
   name                                   = "container-poetry"
