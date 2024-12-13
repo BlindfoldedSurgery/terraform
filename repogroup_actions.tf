1e3ed11ae233a9a019ebeeaf72a0b873858da66b
Björn Petersen <git@bjoernpetersen.net>
2024-12-13 13:53:58 +0100

    action-container: Fix expected job name

--- repogroup_actions.tf
+++ repogroup_actions.tf
@@ -16,11 +16,11 @@ module "actions_container_repo" {
   required_status_checks = [
     "check / check",
     "build-docker-minimal / build",
-    "build-docker-minimal-cached / build",
     "build-docker-all-options ({{defaultContext}}:test-build) / build",
     "build-docker-all-options (test-build) / build",
     "build-docker-all-options (./test-build) / build",
     "build-kaniko-minimal / build",
+    "build-kaniko-minimal-cached / build",
     "build-kaniko-all-options (test-build) / build",
     "build-kaniko-all-options (./test-build) / build",
     "build-kaniko-amd64 / build",
