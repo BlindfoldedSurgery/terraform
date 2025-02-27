1cc61a94adf15e40d6114c0fc6d73d4ec1250d68
Björn Petersen <git@bjoernpetersen.net>
2025-02-27 20:47:19 +0100

    Clean up status checks

--- repogroup_container.tf
+++ repogroup_container.tf
@@ -5,14 +5,7 @@ module "container_flutter_repo" {
   allow_default_branch_protection_bypass = false
   required_status_checks = [
     "check-commits / check",
-    "build-container-image (3.19, amd64) / build",
-    "build-container-image (3.19, arm64) / build",
-    "build-container-image (3.22, amd64) / build",
-    "build-container-image (3.22, arm64) / build",
-    "build-container-image (3.24, amd64) / build",
-    "build-container-image (3.24, arm64) / build",
-    "build-container-image (3.27, amd64) / build",
-    "build-container-image (3.27, arm64) / build",
+    "post-build-container-image",
   ]
 }
 
@@ -23,29 +16,6 @@ module "container_poetry_repo" {
   allow_default_branch_protection_bypass = false
   required_status_checks = [
     "check / check",
-    "build-container-image (3.11, bullseye, installer, amd64) / build",
-    "build-container-image (3.12, bullseye, installer, amd64) / build",
-    "build-container-image (3.13, bullseye, installer, amd64) / build",
-    "build-container-image (3.11, bookworm, installer, amd64) / build",
-    "build-container-image (3.12, bookworm, installer, amd64) / build",
-    "build-container-image (3.13, bookworm, installer, amd64) / build",
-    "build-container-image (3.11, bullseye, pipx, amd64) / build",
-    "build-container-image (3.12, bullseye, pipx, amd64) / build",
-    "build-container-image (3.13, bullseye, pipx, amd64) / build",
-    "build-container-image (3.11, bookworm, pipx, amd64) / build",
-    "build-container-image (3.12, bookworm, pipx, amd64) / build",
-    "build-container-image (3.13, bookworm, pipx, amd64) / build",
-    "build-container-image (3.11, bullseye, installer, arm64) / build",
-    "build-container-image (3.12, bullseye, installer, arm64) / build",
-    "build-container-image (3.13, bullseye, installer, arm64) / build",
-    "build-container-image (3.11, bookworm, installer, arm64) / build",
-    "build-container-image (3.12, bookworm, installer, arm64) / build",
-    "build-container-image (3.13, bookworm, installer, arm64) / build",
-    "build-container-image (3.11, bullseye, pipx, arm64) / build",
-    "build-container-image (3.12, bullseye, pipx, arm64) / build",
-    "build-container-image (3.13, bullseye, pipx, arm64) / build",
-    "build-container-image (3.11, bookworm, pipx, arm64) / build",
-    "build-container-image (3.12, bookworm, pipx, arm64) / build",
-    "build-container-image (3.13, bookworm, pipx, arm64) / build",
+    "post-build-container-image",
   ]
 }
