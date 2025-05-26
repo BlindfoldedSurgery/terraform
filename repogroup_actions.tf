3ac9150360c6a6878e1ab47227167b690ab5bcdf
Björn Petersen <git@bjoernpetersen.net>
2025-05-26 09:44:22 +0200

    Update required status checks for actions-container

--- repogroup_actions.tf
+++ repogroup_actions.tf
@@ -15,20 +15,7 @@ module "actions_container_repo" {
   allow_default_branch_protection_bypass = false
   required_status_checks = [
     "check / check",
-    "build-docker-minimal / build",
-    "build-docker-all-options ({{defaultContext}}:test-build) / build",
-    "build-docker-all-options (test-build) / build",
-    "build-docker-all-options (./test-build) / build",
-    "build-kaniko-minimal / build",
-    "build-dual-kaniko / merge-manifests / create",
-    "build-kaniko-minimal-cached / build",
-    "build-kaniko-all-options (test-build) / build",
-    "build-kaniko-all-options (./test-build) / build",
-    "build-kaniko-amd64 / build",
-    "build-docker-arm64 / build",
-    "clean-package-versions / clean",
-    "merge-with-artifact / create",
-    "merge-with-outputs / create",
+    "all-checks-passed",
   ]
 }
 
