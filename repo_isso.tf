a1d03bab0fe4516c8d2bb93eb06785c26f3d95aa
Björn Petersen <git@bjoernpetersen.net>
2025-10-11 18:34:43 +0200

    chore: add new archival flag to repos

--- repo_isso.tf
+++ repo_isso.tf
@@ -1,12 +1,9 @@
 module "isso_repo" {
-  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.0.1"
-  name                   = "isso"
-  description            = "isso comments for eiersaft.me"
-  required_status_checks = []
-  blocked_branches       = []
-  protect_default_branch = false
-  enable_actions         = false
-  is_archived            = true
-  is_public              = false
-  enable_argocd_rules    = false
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.0.1"
+  name        = "isso"
+  description = "isso comments for eiersaft.me"
+
+  is_archive_prepared = true
+  is_archived         = true
+  is_public           = false
 }
