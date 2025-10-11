3e988aa58464dca7185475ea4030316240618b95
Björn Petersen <git@bjoernpetersen.net>
2025-10-11 18:34:43 +0200

    chore: add new archival flag to repos

--- repo_delivery-rating-provider.tf
+++ repo_delivery-rating-provider.tf
@@ -1,12 +1,9 @@
 module "delivery_rating_provider_repo" {
-  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.0.1"
-  name                   = "delivery-rating-provider"
-  description            = "provide suggestions for a delivery place to try"
-  required_status_checks = []
-  blocked_branches       = []
-  protect_default_branch = false
-  enable_actions         = false
-  is_archived            = true
-  is_public              = false
-  enable_argocd_rules    = false
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.0.1"
+  name        = "delivery-rating-provider"
+  description = "provide suggestions for a delivery place to try"
+
+  is_archive_prepared = true
+  is_archived         = true
+  is_public           = false
 }
--- repo_rancher-monitoring.tf
+++ repo_rancher-monitoring.tf
@@ -1,12 +1,9 @@
 module "rancher-monitoring_repo" {
-  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.0.1"
-  name                   = "rancher-monitoring"
-  description            = "deploy rancher monitoring"
-  required_status_checks = []
-  blocked_branches       = []
-  protect_default_branch = false
-  enable_actions         = false
-  is_archived            = true
-  is_public              = false
-  enable_argocd_rules    = false
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.0.1"
+  name        = "rancher-monitoring"
+  description = "deploy rancher monitoring"
+
+  is_archive_prepared = true
+  is_archived         = true
+  is_public           = false
 }
