fe66b56d3d77785e61cbd95362da2e78ed8f7c9a
Björn Petersen <git@bjoernpetersen.net>
2025-11-07 22:14:20 +0100

    Prepare container-poetry archival

--- repogroup_container.tf
+++ repogroup_container.tf
@@ -9,12 +9,11 @@ module "container_flutter_repo" {
 }
 
 module "container_poetry_repo" {
-  source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
-  name                                   = "container-poetry"
-  description                            = "Container images with Poetry"
-  allow_default_branch_protection_bypass = false
-  required_status_checks = [
-    "check / check",
-    "post-build-container-image",
-  ]
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
+  name        = "container-poetry"
+  description = "Container images with Poetry"
+
+  is_archive_prepared = true
+  is_archived         = false
+  is_public           = false
 }
